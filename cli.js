e86cf5030d11fc8a06d9bae5736a60740b09056d
alestor123 <alestoraldous@gmail.com>
2021-07-22 17:55:16 +0530

    added main cli code

--- cli.js
+++ cli.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+ 
+const netlifyRedirect = require('./App')
+var chalk = require('chalk')
+try { 
+netlifyRedirect(process.argv[2],process.argv[3])
+console.log(chalk.greenBright.bold('Done !!'))
+}
+catch(e) {
+    console.log(chalk.redBright.bold('Oops : '+ e.message))
+}
