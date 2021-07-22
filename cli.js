5c4df39025fc58fb542695c34574c9ac86ffb1ff
alestor123 <alestoraldous@gmail.com>
2021-07-22 17:58:17 +0530

    linted

--- cli.js
+++ cli.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
- 
+
 const netlifyRedirect = require('./App')
-var chalk = require('chalk')
-try { 
-netlifyRedirect(process.argv[2],process.argv[3])
-console.log(chalk.greenBright.bold('Done !!'))
-}
-catch(e) {
-    console.log(chalk.redBright.bold('Oops : '+ e.message))
+const chalk = require('chalk')
+try {
+  netlifyRedirect(process.argv[2], process.argv[3])
+  console.log(chalk.greenBright.bold('Done !!'))
+} catch (e) {
+  console.log(chalk.redBright.bold('Oops : ' + e.message))
 }
